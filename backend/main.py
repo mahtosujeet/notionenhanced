d9a614d2dfaf3302b0905e1cccda912f10d2b5b4
mahtosujeet <sujeetmahto.dev@gmail.com>
2026-03-10 04:03:21 +0530

    add Logging

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,14 @@
+import logging
 from fastapi import FastAPI
+from config.logging_config import setup_logging
 from response_models.checkhealth import CheckHealthResponse
 
-app = FastAPI()
+setup_logging()
+logger = logging.getLogger(__name__)
+logger.info("Logger Config loaded.")
 
+logger.info("Initializing FastAPI...")
+app = FastAPI()
 
 @app.get("/checkhealth", response_model=CheckHealthResponse)
 def read_root():
@@ -11,3 +17,6 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
+
+
+logger.info("FastAPI running...")
