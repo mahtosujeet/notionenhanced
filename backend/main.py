a1babbb616e2bdd019c708fd2a2121f4919f825a
mahtosujeet <sujeetmahto.dev@gmail.com>
2026-03-11 01:21:31 +0530

    add time middleware

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,11 @@
 import logging
-from fastapi import FastAPI
+import time
+
+from fastapi import FastAPI, Request
+
 from config.logging_config import setup_logging
 from response_models.checkhealth import CheckHealthResponse
+
 
 setup_logging()
 logger = logging.getLogger(__name__)
@@ -9,6 +13,14 @@ logger.info("Logger Config loaded.")
 
 logger.info("Initializing FastAPI...")
 app = FastAPI()
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    process_time = time.perf_counter() - start_time
+    response.headers["X-Process-Time"] = f"{process_time: .2f}s"
+    return response
 
 @app.get("/checkhealth", response_model=CheckHealthResponse)
 def read_root():
