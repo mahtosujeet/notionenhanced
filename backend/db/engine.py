1923012a35122ad28d97bfcf02d399f462dd798f
mahtosujeet <sujeetmahto.dev@gmail.com>
2026-03-12 02:51:21 +0530

    refactor: replace context manager for database connections with a simplified session function

--- backend/db/engine.py
+++ backend/db/engine.py
@@ -1,32 +1,21 @@
-from contextlib import contextmanager
+import typing
 
+from fastapi import Depends
 from sqlmodel import create_engine, Session
 
 
 DATABASE_URL = "sqlite:///database.db"
+connect_args = {"check_same_thread": False}
 
 engine = create_engine(
     DATABASE_URL,
+    connect_args=connect_args,
     echo=True
 )
 
 
-@contextmanager
-def get_db_connection():
-    """
-    Context manager for database connections.
-    
-    Usage:
-        with get_db_connection() as session:
-            # Use session for queries
-            ...
-    """
-    session = Session(engine)
-    try:
+def get_session():
+    with Session(engine) as session:
         yield session
-        session.commit()
-    except Exception:
-        session.rollback()
-        raise
-    finally:
-        session.close()
+
+SessionDep = typing.Annotated[Session, Depends(get_session)]
